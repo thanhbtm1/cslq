a223b8985311ebccdc337b65b6c10adbef41eb5d
thanhbtm1 <trancaothanhbtm9@gmail.com>
2026-04-28 23:38:17 +0700

    read from file

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 a = []
-while True:
-    s = input()
+with open(r"C:\Users\LENOVO\Downloads\thanh_cong_2026-04-28T16_17_21.txt", 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+
+for s in lines:
+
     if s == "ok":
         break
     if "BAND : " in s:
@@ -14,7 +18,7 @@ i = 0
 with open('data.txt', 'a', encoding='utf-8') as f:
     for item in a:
         i = i + 1
-        f.write(f"{item}\n") # Ghi xong tự xuống dòng
+        f.write(f"{item}") # Ghi xong tự xuống dòng
 print(f"\033[32m[SUCCESS] Lưu thành công {i} tài khoản\033[0m")
 # print("+-------------------------------------+")
 # for i in range(len(a)):
